ab1c7e2fba575d77b9d833860a2c239b1afbef09
keiomarah <keiomarah@hotmail.com>
2026-04-26 16:01:08 +0200

    initial commit

---
+++ website/auth.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, render_template, request, flash
+
+auth = Blueprint('auth', __name__, template_folder='.templates')
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("login-signup.html", login=True)
+
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        surname = request.form.get('surname')
+        email = request.form.get('email')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(name) < 2:
+            flash('Name must be longer than a character', category='failure')
+        elif len(surname) < 2:
+            flash('Surname must be longer than a character', category='failure')
+        elif len(password1) < 8: 
+            flash('Password must be at least 8 characters', category='failure')
+        elif password1 != password2:
+            flash('Oops! Passwords do not match', category='failure')
+        else: 
+            flash('Account created successfully', category='success')
+        
+    return render_template("login-signup.html", login=False)
